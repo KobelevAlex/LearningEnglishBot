9cf86828ae3782d47742870abf90cf4ac523ddfb
Alexander Kobelev <kobelef@mail.ru>
2025-02-12 22:17:24 +0400

    LEB-05-01-Разработка меню тренажора Подправил код.

--- src/main/kotlin/LEB_04/LEB_04_01.kt
+++ src/main/kotlin/LEB_04/LEB_04_01.kt
@@ -8,21 +8,20 @@ data class Word(
     var correctAnswersCount: Int = 0,
 )
 
-fun main() {
-    val wordsFile: File = File("words.txt")
-    val vocabulary: MutableList<Word> = mutableListOf()
-    fun loadDictionary(): MutableList<Word> {
-        val lines = wordsFile.readLines()
-        for (line in lines) {
-            val lineSplint = line.split("|")
-            val word = Word(lineSplint[0], lineSplint[1], correctAnswersCount = null ?: 0)
-            vocabulary.add(word)
-        }
-        return vocabulary
+val wordsFile: File = File("words.txt")
+val vocabulary: MutableList<Word> = mutableListOf()
+fun loadDictionary(): MutableList<Word> {
+    val lines = wordsFile.readLines()
+    for (line in lines) {
+        val lineSplit = line.split("|")
+        val word = Word(lineSplit[0], lineSplit[1], lineSplit[2].toIntOrNull() ?: 0)
+        vocabulary.add(word)
     }
+    return vocabulary
+}
 
+fun main() {
     val dictionary: MutableList<Word> = loadDictionary()
-
     while (true) {
         println(
             """
