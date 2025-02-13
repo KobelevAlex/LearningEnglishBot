4650ab1616b5dfc767dd977f35ccbe5852b18671
Alexander Kobelev <kobelef@mail.ru>
2025-02-13 19:16:50 +0400

    KTB-06 Реализовать функционал статистики

--- src/main/kotlin/LEB_04/LEB_04_01.kt
+++ src/main/kotlin/LEB_04/LEB_04_01.kt
@@ -33,7 +33,15 @@ fun main() {
         val inputNumber = readln()
         when (inputNumber) {
             "1" -> println("Учить слова")
-            "2" -> println("Статистика")
+            "2" -> {
+                println("Статистика")
+                val listDictionary = dictionary.filter { it.correctAnswersCount >= 3 }
+                val totalCount = vocabulary.count()
+                val learnedCount = listDictionary.count()
+                val percent = (learnedCount * 100) / totalCount
+                println("Выучено $learnedCount из $totalCount слов | $percent%\n")
+            }
+
             "0" -> break
             else -> println("Введите число 1,2 или 0")
         }
