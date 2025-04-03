a5ed6dbb3cc9da564f38d5e5d77623c93a73cc1d
Alexander Kobelev <kobelef@mail.ru>
2025-04-03 21:02:31 +0400

    KTB-16 Разработать кнопки и отправлять их пользователю в телеграм

--- src/main/kotlin/LEB_04/Telegram.kt
+++ src/main/kotlin/LEB_04/Telegram.kt
@@ -33,6 +33,39 @@ class TelegramBotService(private val botToken: String) {
         val response = client.send(request, HttpResponse.BodyHandlers.ofString())
         println("Response: ${response.body()}")
     }
+
+    fun sendMenu(chatId: String) {
+        val url = "${URL_API}bot$botToken/sendMessage"
+        val sendMenuBody = """
+            {
+              "chat_id": $chatId,
+              "text": "Основное меню",
+              "reply_markup": 
+              {
+                 "inline_keyboard": 
+                [
+                  [
+                    { "text": "Изучить слова",
+                      "callback_data": "learn_word_clicked"
+                    },
+                    { "text": "Статистика",
+                      "callback_data": "statistics_clicked"
+                    }
+                  ]
+                ]
+              }
+            }
+        """.trimIndent()
+        val client = httpClient
+        val request = HttpRequest.newBuilder()
+            .uri(URI.create(url))
+            .GET()
+            .header("Content-type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString(sendMenuBody))
+            .build()
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+        println("Response: ${response.body()}")
+    }
 }
 
 fun main(args: Array<String>) {
@@ -42,23 +75,31 @@ fun main(args: Array<String>) {
     val messageTextRegex: Regex = """"text":"(.*?)"""".toRegex()
     val chatIdRegex: Regex = """"chat":\{"id":(\d+)""".toRegex()
     val updateIdRegex: Regex = """"update_id":(\d+)""".toRegex()
+    val dataRegex: Regex =""""data":"(.+?)"""".toRegex()
+    val trainer = LearnWordsTrainer()
     while (true) {
         Thread.sleep(2000)
         val updates: String = telegramBot.getUpdates(updateId)
         val matchResultText = messageTextRegex.find(updates)
         val matchResultChatId = chatIdRegex.find(updates)
         val matchResultUpdateId = updateIdRegex.find(updates)
+        val matchDataRegex = dataRegex.find(updates)
         if (matchResultText != null && matchResultChatId != null && matchResultUpdateId != null) {
             val text = matchResultText.groups[1]?.value
             val chatId = matchResultChatId.groups[1]?.value
+            val data= matchDataRegex?.groups?.get(1)?.value
             if (text != null && chatId != null) {
-                println("Текст сообщения: $text")
                 telegramBot.sendMessage(chatId, text)
-                val newUpdateId = matchResultUpdateId.groups
-                    .get(1)
+                val newUpdateId = matchResultUpdateId.groups[1]
                     ?.value
                     ?.toInt() ?: 0
                 updateId = newUpdateId + 1
+            }
+            if (text?.lowercase() == "/start" && chatId != null) {
+                telegramBot.sendMenu(chatId)
+            }
+            if (data?.lowercase() == "statistics_clicked" && chatId != null) {
+                telegramBot.sendMessage(chatId, "выучено 10 из 10")
             }
         }
     }
